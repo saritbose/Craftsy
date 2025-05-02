d0b5a7f5d42908fc64f83736b65cb8f1669fd3ea
Sarit <saritbose26@gmail.com>
2025-05-02 12:12:39 +0530

    designing the home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,83 +1,93 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { AlignJustify, Search } from "lucide-react";
+import Cards from "@/Component/Cards";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AlignJustify, Box, Search } from "lucide-react";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      {/* business name, sign up, navigation */}
-      <header className="flex justify-between">
-        <div className="flex">
-          <AlignJustify />
-          Craftsy
+    <div className="overflow-hidden">
+      {/* Header */}
+      <div className="flex justify-between sm:justify-around m-2">
+        <div className="flex gap-1 ">
+          <Box />
+          <div className="hidden sm:block">Craftsy</div>
         </div>
-        <div className="flex">
-          Sign up
-          <Search />
+        <div className="hidden sm:flex gap-8">
+          <div>How It Works</div>
+          <div>Categories</div>
+          <div>Find a Designer</div>
+          <div>Studio</div>
         </div>
-      </header>
-
-      {/* Search bar */}
-      <div
-        className="font-medium font-serif text-white "
-        style={{
-          backgroundImage: "url('/ApartmentCoderSm.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "60vh",
-        }}
-      >
-        <p>Connecting clients in need to freelancers who deliver</p>
-        <div>
-          <Tabs defaultValue="findtalent" className="w-[400px]">
-            <TabsList>
-              <TabsTrigger value="findtalent">Find talent</TabsTrigger>
-              <TabsTrigger value="browsejobs">Browse jobs</TabsTrigger>
-            </TabsList>
-            <TabsContent value="findtalent">
-              <div className="flex relative">
-                <Input placeholder="Search by roles, skills, or keywords" />
-                <Search className="absolute right-20" />
-                <Button className="absolute right-1">Search</Button>
-              </div>
-              <div>Companies</div>
-            </TabsContent>
-            <TabsContent value="browsejobs">
-              <p>
-                Build your freelancing career on Upwork, with thousands of jobs
-                posted every week.
-              </p>
-              <Button>Explore recently posted jobs</Button>
-            </TabsContent>
-          </Tabs>
+        <div className="sm:flex gap-6">
+          <div className="hidden sm:block">Log in</div>
+          <div className="hidden sm:block">Sign up</div>
+          <AlignJustify className="sm:hidden" />
         </div>
       </div>
 
-      {/* Advantages of using craftsy */}
-      <div>
-        <div></div>
-        <div></div>
+      {/* Start Point */}
+      <div className="flex flex-col sm:flex-row">
+        <div className=" w-full sm:w-1/2 px-6 py-10 flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-5xl font-bold mb-4">
+            World Class <span className="text-blue-600">Design At</span> Your
+            Service
+          </h1>
+          <p className="text-gray-600 mb-6">
+            Find great talent. Build your business. Take your career to the next
+            level.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-3">
+            <input
+              type="text"
+              placeholder="   Logo,Website,Branding..."
+              className="hidden sm:block rounded-4xl border border-gray-300 w-full sm:w-auto flex-1"
+            />
+            <button className="bg-orange-500 text-white px-6 py-3 rounded-4xl">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div className="w-full sm:w-[55%] sm:h-2/4">
+          <img src="/Picccky.png" className="w-full h-full mx-3" />
+        </div>
       </div>
 
-      {/* Browse talent by category */}
-      <div></div>
-
-      {/* More advantages with pic, forcing to signup/signin */}
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-
-      {/* reviews */}
-      <div></div>
-
-      {/* category search selection */}
-      <div></div>
-
-      {/* footers */}
-      <div></div>
+      {/* Advantages */}
+      <div className="bg-neutral-100 my-10">
+        <div className="text-center text-2xl py-10">
+          It's Easy To Get Work Done On Creative Peoples
+        </div>
+        <div className="flex flex-col sm:flex-row justify-around">
+          <Cards
+            title={"Post a Job"}
+            content={
+              "Create your free job posting and start receiving Quotes within hours."
+            }
+            icon={Search}
+          />
+          <Cards
+            title={"Hire Freelancers"}
+            content={
+              "Compare the Quotes you receive and hire the best freelance professionals for the job."
+            }
+            icon={Search}
+          />
+          <Cards
+            title={"Get Work Done"}
+            content={
+              "Decide on how and when payments will be made and use WorkRooms to collaborate."
+            }
+            icon={Search}
+          />
+          <Cards
+            title={"Pay Safely"}
+            content={
+              "Choose from multiple payment methods with Stripe payment protection."
+            }
+            icon={Search}
+          />
+        </div>
+      </div>
     </div>
   );
 };
