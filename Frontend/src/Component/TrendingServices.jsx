a702305671bd881f4929f7f8d40e119323292840
Sarit <saritbose26@gmail.com>
2025-05-03 12:34:59 +0530

    Landing Page Designing only html+tailwindcss completed, added services,testimonials,footer

--- Frontend/src/Component/TrendingServices.jsx
+++ Frontend/src/Component/TrendingServices.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const TrendingServices = ({ Service, image }) => {
   return (
-    <div className="bg-orange-500 rounded-md w-32 flex-shrink-0 text-white">
+    <div className="bg-orange-500 shadow-md rounded-md w-32 flex-shrink-0 text-white">
       <div className="p-1 h-45 items-start justify-between flex flex-col">
         <h2 className="text-xl font-semibold mb-4">{Service}</h2>
-        <div className="bg-orange-300 rounded-xl w-full">
+        <div className=" rounded-xl w-full">
           <img src={image} className="rounded-lg w-full object-cover" />
         </div>
       </div>
