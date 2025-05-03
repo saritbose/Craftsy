87ac901892bb52c17afb7ef1ca50ba2244ce8586
Sarit <saritbose26@gmail.com>
2025-05-03 05:43:40 +0530

    adding new designs and redesigning old designs

--- Frontend/src/Component/Cards.jsx
+++ Frontend/src/Component/Cards.jsx
@@ -1,14 +1,12 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 
 const Cards = ({ title, content, icon: Icon }) => {
   return (
-    <Card className="w-[20%] min-h-[5%] rounded-3xl text-center justify-around hover:bg-orange-500 hover:text-white">
-      <CardHeader>
-        {<Icon />}
-        <CardTitle className="font-bold text-[15px]">{title}</CardTitle>
-      </CardHeader>
-      <CardContent className="text-[10px] text-neutral-500">
+    <Card className="w-[40%] h-[10%] rounded-3xl text-center items-center justify-center hover:bg-orange-500 hover:text-white group">
+      <CardContent>{<Icon className="" />}</CardContent>
+      <CardContent className="font-bold text-1vh">{title}</CardContent>
+      <CardContent className="text-[10px] text-neutral-500 group-hover:text-white">
         {content}
       </CardContent>
     </Card>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import Cards from "@/Component/Cards";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { AlignJustify, Box, Search } from "lucide-react";
+import {
+  AlignJustify,
+  ArrowRight,
+  Box,
+  Briefcase,
+  CheckCircle,
+  ChevronDown,
+  ChevronUp,
+  CreditCard,
+  Laptop2,
+  Search,
+  ThumbsUp,
+} from "lucide-react";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       {/* Header */}
-      <div className="flex justify-between sm:justify-around m-2">
+      <div className="flex justify-between m-2">
         <div className="flex gap-1 ">
           <Box />
           <div className="hidden sm:block">Craftsy</div>
@@ -48,44 +60,197 @@ const Home = () => {
           </div>
         </div>
         <div className="w-full sm:w-[55%] sm:h-2/4">
-          <img src="/Picccky.png" className="w-full h-full mx-3" />
+          <img src="/Picccky.png" className="w-full h-full px-5" />
         </div>
       </div>
 
       {/* Advantages */}
-      <div className="bg-neutral-100 my-10">
-        <div className="text-center text-2xl py-10">
+      <div className="bg-neutral-100 my-10 pb-10">
+        <div className="text-center text-3xl py-10 font-semibold">
           It's Easy To Get Work Done On Creative Peoples
         </div>
-        <div className="flex flex-col sm:flex-row justify-around">
+        <div className="flex flex-1/2 flex-col md:flex-row mx-5 md:flex-wrap items-center justify-center  gap-10">
           <Cards
             title={"Post a Job"}
             content={
               "Create your free job posting and start receiving Quotes within hours."
             }
-            icon={Search}
+            icon={Briefcase}
           />
           <Cards
             title={"Hire Freelancers"}
             content={
               "Compare the Quotes you receive and hire the best freelance professionals for the job."
             }
-            icon={Search}
+            icon={Laptop2}
           />
           <Cards
             title={"Get Work Done"}
             content={
               "Decide on how and when payments will be made and use WorkRooms to collaborate."
             }
-            icon={Search}
+            icon={ThumbsUp}
           />
           <Cards
             title={"Pay Safely"}
             content={
               "Choose from multiple payment methods with Stripe payment protection."
             }
-            icon={Search}
+            icon={CheckCircle}
           />
+        </div>
+      </div>
+
+      {/* Call to action */}
+      <div className="flex flex-col sm:flex-row">
+        <div className="w-full sm:w-[55%] sm:h-2/4">
+          <img src="/Picccky2.png" className="w-full h-full px-5" />
+        </div>
+        <div className=" w-full sm:w-1/2 px-6 py-10 flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-5xl font-semibold mb-4">
+            Two Ways To Get Design
+          </h1>
+          <p className="text-gray-900 mb-6">
+            Our tried-and-true creative process makes design magic. With our
+            global community that loves to collaborate, we'll turn your great
+            ideas.
+          </p>
+          <div className="flex flex-col gap-3">
+            <div>
+              <div className="flex justify-between bg-blue-700 rounded-md p-2 mb-1">
+                <div className="flex items-center pb-5 ">
+                  <div className="rounded-full w-9 h-10 p-2 font-bold text-center bg-orange-200 text-orange-700">
+                    1
+                  </div>
+                  <div className="ml-3 font-bold">Work Directly</div>
+                </div>
+                <div className="">
+                  <ChevronDown />
+                  <ChevronUp />
+                </div>
+              </div>
+              <div className="flex justify-between bg-blue-700 rounded-md p-2 mb-1">
+                <div className="flex items-center pb-5">
+                  <div className="rounded-full w-9 h-10 p-2 font-bold text-center bg-orange-200 text-orange-700">
+                    2
+                  </div>
+                  <div className="ml-3 font-bold">Start a Contest</div>
+                </div>
+                <div>
+                  <ChevronDown />
+                  <ChevronUp />
+                </div>
+              </div>
+            </div>
+            <div className="flex">
+              <button className="bg-orange-500 text-white px-6 py-3 rounded-4xl">
+                Get Started
+              </button>
+              <div className=" flex gap-1 px-6 py-3 hover:underline">
+                How it Works
+                <ArrowRight />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Services */}
+      <div className="my-10 pb-10">
+        <div className="text-center text-3xl py-10 font-semibold">
+          Trending Professional Services
+        </div>
+        <div className="flex justify-evenly overflow-x-scroll scrollbar-hide gap-5 mx-5">
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
+          <div className=" flex flex-col bg-green-950 rounded-md min-w-[30%] h-full">
+            <div className="text-white m-2 font-bold text-xl">Services</div>
+            <div>
+              <img src="/Picccky.png" className="rounded-md p-1" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
