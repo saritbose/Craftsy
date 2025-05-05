54ce39f7fa683da8e21ded0d6e1de4390e89755e
Sarit <saritbose26@gmail.com>
2025-05-05 10:56:26 +0530

    Job Board Frontend Part Done, Left Backend Part

---
+++ Frontend/src/Component/Jobs.jsx
@@ -0,0 +1,35 @@
+import { MapPin } from "lucide-react";
+import React from "react";
+
+const Jobs = () => {
+  return (
+    <div className="pt-5 pb-3 px-4 group hover:bg-gray-100">
+      <p className="text-neutral-800 text-xs mb-1">Posted Date</p>
+      <p className="text-xl font-semibold hover:underline group-hover:text-orange-400">
+        Job Title
+      </p>
+      <p className="text-neutral-800 text-xs my-2">
+        Price structure - Level - Est. Budget: $
+      </p>
+      <p className="font-light">
+        Short job description...
+        <span className="text-orange-400 underline">more</span>
+      </p>
+      <p className="rounded-2xl bg-neutral-200 w-fit px-3 py-0.5 mb-4 my-3 text-neutral-500">
+        Skill
+      </p>
+      <p className="flex gap-1 text-neutral-500">
+        <MapPin className="w-4 h-5 text-neutral-700" />
+        Country
+      </p>
+      <p className="text-gray-500 mt-1 mb-4">
+        Proposals:<span className="text-neutral-500"> Applicants</span>
+      </p>
+      <button className="bg-orange-300 p-1.5 px-3 rounded-3xl hover:text-white hover:bg-orange-400">
+        Apply
+      </button>
+    </div>
+  );
+};
+
+export default Jobs;
---
+++ Frontend/src/pages/FreelancerDashboard.jsx
@@ -0,0 +1,157 @@
+import Jobs from "@/Component/Jobs";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  AlignJustify,
+  ArrowDown,
+  Bell,
+  ChevronDown,
+  CircleHelp,
+  ExternalLink,
+  Search,
+  X,
+} from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const FreelancerDashboard = () => {
+  const [nav, setNav] = useState(false);
+
+  return (
+    <div className="p-3">
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <div>
+          <div onClick={() => setNav(!nav)}>
+            {nav === true ? (
+              <>
+                <X className="sm:hidden m-2" />
+                <div className="fixed top-14 left-0 bg-white w-[50%] border-2 shadow-md">
+                  <div className="m-5">Browse Jobs</div>
+                  <div className="m-5">Your Applications</div>
+                  <div className="m-5">Ongoing Work</div>
+                  <div className="m-5">Earnings</div>
+                  <hr className="w-full bg-neutral-400 " />
+                  <div className="my-5 mx-3 flex flex-col gap-3">
+                    <Link
+                      to={"/login"}
+                      className="hover:bg-orange-400 hover:text-white text-center p-2 rounded-3xl w-fit"
+                    >
+                      My Profile
+                    </Link>
+                    <Link
+                      to={"/register"}
+                      className="hover:bg-orange-400 hover:text-white text-center p-2 rounded-3xl w-fit"
+                    >
+                      Log out
+                    </Link>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <AlignJustify className="sm:hidden m-2" />
+            )}
+          </div>
+        </div>
+        <Link to={"/"}>Craftsy</Link>
+        <div className="hidden md:flex gap-5 items-center">
+          <div className="group relative">
+            <div className="flex hover:text-orange-300">
+              Find work <ChevronDown className="translate-y-0.5" />
+            </div>
+            <div className="hidden group-hover:block absolute bg-white mt-1 py-2 w-64 text-sm border rounded-xl shadow-md">
+              <p className="pb-1 px-3 hover:bg-gray-100">Find work</p>
+              <p className="pb-1 px-3 hover:bg-gray-100">Saved jobs</p>
+              <p className="pb-2 px-3 hover:bg-gray-100">Proposals & offers</p>
+              <hr className="w-1vw pb-1 " />
+              <p className="pb-1 px-3 text-neutral-400">Reach more clients</p>
+              <p className="pb-1 px-3 hover:bg-gray-100 flex gap-1">
+                Promote with ads
+                <ExternalLink className="w-4 h-6" />
+              </p>
+              <p className="pb-1 px-3 hover:bg-gray-100">Direct Contracts</p>
+            </div>
+          </div>
+          <div className="group relative">
+            <div className="flex hover:text-orange-300">
+              Deliver work <ChevronDown className="translate-y-0.5" />
+            </div>
+            <div className="hidden group-hover:block absolute bg-white mt-1 py-2 w-64 text-sm border rounded-xl shadow-md">
+              <p className="pb-2 px-3 hover:bg-gray-100">
+                Your active contracts
+              </p>
+              <p className="pb-2 px-3 hover:bg-gray-100">Contract history</p>
+              <p className="pb-2 px-3 hover:bg-gray-100">Hourly work diary</p>
+            </div>
+          </div>
+          <div className="group relative">
+            <div className="flex hover:text-orange-300">
+              Manage finances <ChevronDown className="translate-y-0.5" />
+            </div>
+            <div className="hidden group-hover:block absolute bg-white mt-1 py-2 w-64 text-sm border rounded-xl shadow-md">
+              <p className="pb-2 px-3 hover:bg-gray-100">Financial overview</p>
+              <p className="pb-2 px-3 hover:bg-gray-100">Your reports</p>
+              <p className="pb-2 px-3 hover:bg-gray-100">
+                Billings and earnings
+              </p>
+              <p className="pb-2 px-3 hover:bg-gray-100">
+                Transactions and invoices
+              </p>
+              <hr className="w-1vw pb-1 " />
+              <p className="pb-2 px-2 text-neutral-400">Payments</p>
+              <p className="pb-2 px-3 hover:bg-gray-100">Withdraw earnings</p>
+            </div>
+          </div>
+          <div className="hover:text-orange-300">Messages</div>
+        </div>
+        <div className="flex items-center gap-5">
+          <Search />
+          <CircleHelp className="hidden md:block" />
+          <Bell className="hidden md:block" />
+          <Button className="rounded-full hidden md:block" />
+        </div>
+      </div>
+
+      {/* Job boards */}
+      <div className="mx-2">
+        <p className="font-medium my-5 text-xl">Jobs you might like</p>
+        <Tabs defaultValue="bestmatches">
+          <TabsList className="bg-white">
+            <TabsTrigger value="bestmatches">Best Matches</TabsTrigger>
+            <TabsTrigger value="mostrecent">Most Recent</TabsTrigger>
+            <TabsTrigger value="savedjobs">Saved Jobs</TabsTrigger>
+          </TabsList>
+          <TabsContent value="bestmatches">
+            <p className="text-gray-600 my-3">
+              Browse jobs that match your experience to a client's hiring
+              preferences. Ordered by most relevant.
+            </p>
+            <hr className="w-1vw mt-2" />
+            <div>
+              {/* Jobs */}
+              <Jobs />
+              <Jobs />
+            </div>
+          </TabsContent>
+          <TabsContent value="mostrecent">
+            <p className="text-gray-600 my-3">
+              Browse the most recent jobs that match your skills and profile
+              description to the skills clients are looking for.
+            </p>
+            <hr className="w-1vw mt-2" />
+            <div>
+              {/* Jobs */}
+              <Jobs />
+              <Jobs />
+            </div>
+          </TabsContent>
+          <TabsContent value="savedjobs" className="text-gray-600 my-3">
+            Feature still not available.
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default FreelancerDashboard;
